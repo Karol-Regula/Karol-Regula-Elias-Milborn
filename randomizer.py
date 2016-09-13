06b80e9b6e137e382a91dfa752bcf30b1b10c37a
Elias Milborn <emilborn@stuy.edu>
2016-09-13 15:50:03 -0400

    added printDict method, should be fully functional

--- randomizer.py
+++ randomizer.py
@@ -1,3 +1,5 @@
+import random
+
 dict =   {4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson', 'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin', 'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth', 'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj', 'Amy', 'Arvind', 'Nobel', 'Angel\
 a', 'Edward', 'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],                                                                                                                                                                 
     8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki', 'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda', 'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent', 'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel', 'Felix', 'Jacob',\
@@ -5,8 +7,11 @@ a', 'Edward', 'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
     9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C', 'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha', 'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry', 'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine', 'Jessica', 'Kate', 'Sham\
 aul', 'Max', 'Sarah', 'Anthony L', 'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']     }                                                                                                                                           
 
-#print (dict[4]:randint(0,len(dict[4])))
-#This is essentially what we need, just have to figure out the syntax, I get home at 8pm
-print (dict[8])
-print (dict[9])
-(randint(0,9))
+def randomDict(entry):#takes an period number as input
+    print('printing random student from period ' + str(entry))
+    print (dict[entry][random.randint(0, len(dict[entry]) - 1)])
+
+randomDict(4)
+randomDict(8)
+randomDict(9)
+
